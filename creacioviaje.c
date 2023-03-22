2d521b461077794dc2283d7eb29ee61ad3443795
elxelu <127350032+elxelu@users.noreply.github.com>
2023-03-22 17:24:38 +0100

    Update creacioviaje.c

--- creacioviaje.c
+++ creacioviaje.c
@@ -1,48 +1,68 @@
+//esto lo ha realizado jose luis ramirez tocino
 #include <stdio.h>
+#include <time.h> // libreria de control del tiempo
 
+/* esto es una guia del formato del struct
 struct viaje
 {
 	int idviaje;
-	int idusuario;
-	char matricula[];
+	char idusuario;
+	char matricula[TamMatri];
 	int plazalibre;
 	int idavuel;
-	char estado[];
-	char fecha[];
-	char horasal[];
-	char horalle[];
+	char estado[TamEstado];
+	char fecha[tamFecha];
+	char horasal[TamHora];
+	char horalle[TamHora];
 	float totalprec;
 }
-
+*/
+char hora;
 struct viaje *crearvi;
 
 crearvi=(struct viaje *)realloc(sizeof(struct viaje )* 1);
 
-{
+{ #comienzo del realloc
 
-$crearvi[idviaje].plazas="campo plazar del fichero vehiculos"; # estableze el numero de plazas disponibles
+&crearvi[idviaje].idviaje=idviaje; # establece el idviaje
+	
+&crearvi[idviaje].idusuario=idusuario; # estable el idusuario igual que el id del usuario que he creado el viaje
+	
+&crearvi[idviaje].plazas="campo plaza del fichero vehiculos"; # establece el numero de plazas disponibles segun el vehiculo selecionado
 
 printf("ida o vuelta"); #pide que el usuario indique si el viaje es de ida o vuelta
 scanf("$c",&idvuelt);
 if($idavuelt=="ida"){
-    $crearvi[idviaje].idavuel=1;
+    &crearvi[idviaje].idavuel=1;
 }else{
-    $crearvi[idviaje].idavuel=0;
+    &crearvi[idviaje].idavuel=0;
 }
 
-$crearvi[idviaje].estado="abierto"; # estabece por defecto el viaje como abierto
+&crearvi[idviaje].estado="abierto"; # estabece por defecto el viaje como abierto
 
 print("introduzca fecha"); #pide la fecha que el viaje !! hay que controlar que la fecha no sea del pasado!!
 scanf("$c",&crearvi[idviaje].fecha);
+	if(&crearvi[idviaje].fecha< 
 
 print("introduzca hora de salida"); #pide la hora de salida !! hay que controlar que la hora no haya pasado
-scanf("$c",&crearvi[idviaje].horasal);
-
+scanf("$c",&hora);
+	   while(hora<=time(&now){ # controlador hora salida
+	   printf("hora no posible"); 
+	   scanf("$c",&hora);
+	   }
+&crearvi[idviaje].horasal=hora;
+		 
 print("introduzca hora de llegada"); # pide la hora de llegada !! hay que controlar que sea mayor que la de salida
-scanf("$c",&crearvi[idviaje].horalle);
+scanf("$c",&hora);
+	   while(hora<=crearvi[idviaje].horasal){ # controlador hora llegada
+	   printf("hora no posible"); 
+	   scanf("$c",&hora);
+	   }
+&crearvi[idviaje].horalle=hora;
+	      
 
 printf("introduce precio total del viaje persona"); # pide el precio total del viaje
 scanf("$f",&crearvi[idviaje].totalprecio);
 
-}
+} #fin del realoc		
 //
