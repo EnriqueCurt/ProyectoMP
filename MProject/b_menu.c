f9672c894cf8c19284711ba6e09cddfdc6a432d1
DESKTOP-0GON66O\fanru <fangrui.cao@alum.uca.es>
2023-04-12 17:36:59 +0200

    LA funcion listar

--- MProject/b_menu.c
+++ MProject/b_menu.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "struct_ficheros.h"
 
+void listaUsuarios(){
+    printf("Estos son todos los usuarios");
+    for (int i = 0; i < num_usuarios; i++) {
+        printf("Usuario %d: id=%s, nombre=%s, localidad=%s, admin=%d, cuenta=%s, contra=%s\n",
+               i+1,
+               usuarios[i].idusuario,
+               usuarios[i].nombre,
+               usuarios[i].localidad,
+               usuarios[i].administrador,
+               usuarios[i].cuenta,
+               usuarios[i].contra);
+    }
+}
+
+void listarVehiculos(){
+    printf("Estos son todos los viajes\n");
+     for (int j = 0; j < num_vehiculos; j++) {
+        printf("Id usuario %s Matricula=%s, plazas=%d , descripcion=%s \n",
+               vehiculos[j].idusuario,
+               vehiculos[j].matricula,
+               vehiculos[j].plazas,
+               vehiculos[j].descript);
+    }
+}
+
+void listarViajes(){
+    printf("Esto son todos los viajes\n");
+     for (int j = 0; j < num_vehiculos; j++) {
+        printf("Id viaje=%s, Matricula=%s,fecha=%02d/%02d/%d , hora salida=%02d:%02d, hora llegada=%02d:%02d, ida o vuelta=%d, plazas=%d, precio=%.2f$, estado=%s \n",
+               viajes[j].idviaje,
+               viajes[j].matricula,
+               viajes[j].fecha.dia,
+               viajes[j].fecha.mes,
+               viajes[j].fecha.ano,
+               viajes[j].horasalida.horas,
+               viajes[j].horasalida.minutos,
+               viajes[j].horallegada.horas,
+               viajes[j].horallegada.minutos,
+               viajes[j].idavuelta,
+               viajes[j].plazas,
+               viajes[j].precio,
+               viajes[j].estado);
+    }
+}
--- MProject/b_menu.h
+++ MProject/b_menu.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+void listaUsuarios();
+void listarVehiculos();
+void listarViajes();
 
 
 
